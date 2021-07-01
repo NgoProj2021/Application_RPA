ebaa16e058633428f685d0cc0443a286b13ecbdb
NgoProj2021 <82966891+NgoProj2021@users.noreply.github.com>
2021-07-02 01:17:41 +0530

    Add files via upload

--- src/main/java/com/ngo/controllers/ProjectSectionsController.java
+++ src/main/java/com/ngo/controllers/ProjectSectionsController.java
@@ -1,37 +1,18 @@
 package com.ngo.controllers;
 
-import com.ngo.entity.ProjectSections;
-import com.ngo.entity.Test;
-import com.ngo.services.projectsection.ProjectSectionService;
-import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.ngo.services.ProjectSectionServiceImpl;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigInteger;
-import java.util.List;
 
 
 @RestController
-@RequestMapping("/v1/projectSection")
-@Slf4j
 public class ProjectSectionsController {
-    @Autowired
-    private ProjectSectionService projectSectionService;
+    private ProjectSectionServiceImpl projectSectionServiceImpl;
+    public ProjectSectionsController(ProjectSectionServiceImpl projectSectionServiceImpl){
+        this.projectSectionServiceImpl=projectSectionServiceImpl;
+    }
 
-    @GetMapping("/project")
-    @ResponseBody
-    public ResponseEntity<List<ProjectSections>> getProjectSections(){
-        log.info("getProjectSections ");
-        BigInteger headerId=BigInteger.valueOf(10);
-        List<ProjectSections>  resultList=  projectSectionService.getAllProjectSections(headerId);
-        ResponseEntity<List<ProjectSections>> responseEntity = new ResponseEntity<>(resultList, HttpStatus.OK);
-        return responseEntity;
+    public void getProjectSections(){
+
     }
 
 }
--- src/main/java/com/ngo/entity/ProjectSections.java
+++ src/main/java/com/ngo/entity/ProjectSections.java
@@ -1,9 +1,5 @@
 package com.ngo.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -12,19 +8,13 @@ import java.math.BigInteger;
 
 @Entity
 @Table(name = "ORGANIZATION_PROJECT_SECTIONS")
-@Getter
-@Setter
-@NoArgsConstructor
 public class ProjectSections {
 
-
-    @Id
     @Column(name="PROJECT_SECTION_ID")
+    @Id
     private BigInteger projectSectionId;
     @Column(name="ORGANIZATION_PROJECT_HEADER_ID")
     private BigInteger projectHeaderId;
     @Column(name="PROJECT_SECTION_NAME")
     private String projectSectionName;
-
-
 }
